ce33b07d4e7bfc4e6e8438570e255e9da246ff5e
rakshaxworkz <shettyraksha360@gmail.com>
2024-07-03 10:29:30 +0530

    AllenSoll

---
+++ AllenSollyWomensTShirt.java
@@ -0,0 +1,9 @@
+class AllenSollyWomensTShirt{
+public static void main(String[] tshirt){
+System.out.println("Occasion : Casual");
+System.out.println("Fit : Regular Fit");
+System.out.println("Material : 95% Cotton and 5% Elastane");
+System.out.println("Neck : Polo Neck; Pattern : Textured");
+System.out.println("Material Composition: 95% Cotton, 5% Elastane; Sleeve Type: Half Sleeve; Material Type: Synthetic; Collar Style: Spread Collar; Sport Type: Outdoor Lifestyle");
+}
+}
