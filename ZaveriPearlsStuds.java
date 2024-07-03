8f0fb1f2d6b1407baa2d4f667ed085cc40dfe2dc
rakshaxworkz <shettyraksha360@gmail.com>
2024-07-03 10:59:51 +0530

    Add files via upload

---
+++ ZaveriPearlsStuds.java
@@ -0,0 +1,10 @@
+class ZaveriPearlsStuds{
+public static void main(String[] studs){
+System.out.println("Brand: ZAVERI PEARLS");
+System.out.println("Color: Gold");
+System.out.println("Plating: 22K Yellow Gold");
+System.out.println("Material: Alloy");
+System.out.println("Stones: Artificial Stones");
+System.out.println("Features: If You are Looking For The Perfect Earrings To Complete A Specific Look Or Just To Add Some Flare To Your Everyday Jewelry Game Or To Elevate Your Office Wardrobe, Well!!!! You Are At Right Place. Timeless And Easy On The Eyes, this set of 25 Earrings Are Easy To Blend With, Yet Refined Enough To Add A Dignified Presence. Creating A Simple And Classic Look, These Earrings Will Give Your Outfit The Wow Factor You Desire.");
+}
+}
