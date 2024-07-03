9c6ee3651d891156814717fe6862e7c30c136fe1
rakshaxworkz <shettyraksha360@gmail.com>
2024-07-03 10:52:47 +0530

    Added java file

---
+++ HighStarWomenSweater.java
@@ -0,0 +1,8 @@
+class HighStarWomenSweater{
+public static void main(String[] sweater){
+System.out.println("Material Type : 100% Acrylic");
+System.out.println("Neck : Turtle Neck");
+System.out.println("Care Instructions: Machine Wash");
+System.out.println("Sleeve : Long Sleeve");
+}
+}
