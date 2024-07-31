97b1e68f602475ee2336a1f9307339ce7dbc3271
rakshaxworkz <shettyraksha360@gmail.com>
2024-07-31 19:48:13 +0530

    Committing the assignment on Instance Implementation

---
+++ SocialMediaRunner.java
@@ -0,0 +1,111 @@
+class SocialMediaRunner{
+
+    public static void main(String sM[]){
+	
+	    System.out.println("Main Started");
+	    SocialMedia ref = new SocialMedia();
+		ref.socialMediaId = 657823;
+		ref.socialMediaName = "Snapchat";
+		ref.profileName = "Kruthika";
+		ref.emailId = "kruthi23@gmail.com";
+		System.out.println("Social Media Account Generated");
+		System.out.println("Social Media Id is " + ref.socialMediaId);
+		System.out.println("Social Media Name is " + ref.socialMediaName);
+		System.out.println("Profile Name is " + ref.profileName);
+		System.out.println("Email Id is " + ref.emailId);
+		
+		SocialMedia ref1 = new SocialMedia();
+		ref1.socialMediaId = 7636698;
+		ref1.socialMediaName = "Facebook";
+		ref1.profileName = "KruthikaKruthika";
+		ref1.emailId = "kruthikruthi123@gmail.com";
+		System.out.println("Social Media Account Generated");
+		System.out.println("Social Media Id is " + ref1.socialMediaId);
+		System.out.println("Social Media Name is " + ref1.socialMediaName);
+		System.out.println("Profile Name is " + ref1.profileName);
+		System.out.println("Email Id is " + ref1.emailId);
+		
+		SocialMedia ref2 = new SocialMedia();
+		ref2.socialMediaId = 687986;
+		ref2.socialMediaName = "Instagram";
+		ref2.profileName = "Bhoomika";
+		ref2.emailId = "bhoomi65@gmail.com";
+		System.out.println("Social Media Account Generated");
+		System.out.println("Social Media Id is " + ref2.socialMediaId);
+		System.out.println("Social Media Name is " + ref2.socialMediaName);
+		System.out.println("Profile Name is " + ref2.profileName);
+		System.out.println("Email Id is " + ref2.emailId);
+		
+		SocialMedia ref3 = new SocialMedia();
+		ref3.socialMediaId = 76689;
+		ref3.socialMediaName = "Twitter";
+		ref3.profileName = "Anitha";
+		ref3.emailId = "ani@gmail.com";
+		System.out.println("Social Media Account Generated");
+		System.out.println("Social Media Id is " + ref3.socialMediaId);
+		System.out.println("Social Media Name is " + ref3.socialMediaName);
+		System.out.println("Profile Name is " + ref3.profileName);
+		System.out.println("Email Id is " + ref3.emailId);
+		
+		SocialMedia ref4 = new SocialMedia();
+		ref4.socialMediaId = 65238;
+		ref4.socialMediaName = "LinkedIn";
+		ref4.profileName = "Shashi";
+		ref4.emailId = "shashi87@gmail.com";
+		System.out.println("Social Media Account Generated");
+		System.out.println("Social Media Id is " + ref4.socialMediaId);
+		System.out.println("Social Media Name is " + ref4.socialMediaName);
+		System.out.println("Profile Name is " + ref4.profileName);
+		System.out.println("Email Id is " + ref4.emailId);
+		
+		SocialMedia ref5 = new SocialMedia();
+		ref5.socialMediaId = 23681;
+		ref5.socialMediaName = "Github";
+		ref5.profileName = "Keerthi Kumar";
+		ref5.emailId = "keerthi24@gmail.com";
+		System.out.println("Social Media Account Generated");
+		System.out.println("Social Media Id is " + ref5.socialMediaId);
+		System.out.println("Social Media Name is " + ref5.socialMediaName);
+		System.out.println("Profile Name is " + ref5.profileName);
+		System.out.println("Email Id is " + ref5.emailId);
+		
+		SocialMedia ref6 = new SocialMedia();
+		ref6.socialMediaId = 74922;
+		ref6.socialMediaName = "Amazon";
+		ref6.profileName = "Kanika";
+		ref6.emailId = "kani789@gmail.com";
+		System.out.println("Social Media Account Generated");
+		System.out.println("Social Media Id is " + ref6.socialMediaId);
+		System.out.println("Social Media Name is " + ref6.socialMediaName);
+		System.out.println("Profile Name is " + ref6.profileName);
+		System.out.println("Email Id is " + ref6.emailId);
+		
+		SocialMedia ref7 = new SocialMedia();
+		ref7.socialMediaId = 726861;
+		ref7.socialMediaName = "Meesho";
+		ref7.profileName = "Mithun";
+		ref7.emailId = "mithu56@gmail.com";
+		System.out.println("Social Media Account Generated");
+		System.out.println("Social Media Id is " + ref7.socialMediaId);
+		System.out.println("Social Media Name is " + ref7.socialMediaName);
+		System.out.println("Profile Name is " + ref7.profileName);
+		System.out.println("Email Id is " + ref7.emailId);
+		
+		SocialMedia ref8 = new SocialMedia();
+		ref8.socialMediaId = 38271;
+		ref8.socialMediaName = "Shopsy";
+		ref8.profileName = "Ravi";
+		ref8.emailId = "ravi@gmail.com";
+		System.out.println("Social Media Account Generated");
+		System.out.println("Social Media Id is " + ref8.socialMediaId);
+		System.out.println("Social Media Name is " + ref8.socialMediaName);
+		System.out.println("Profile Name is " + ref8.profileName);
+		System.out.println("Email Id is " + ref.emailId);
+		System.out.println("Main Ended");
+		
+		
+	
+	}
+
+
+}
