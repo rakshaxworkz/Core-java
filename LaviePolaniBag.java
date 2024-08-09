166f68d2a43a7d235b162b7aa7b53bf0801de3da
rakshaxworkz <shettyraksha360@gmail.com>
2024-08-09 11:47:30 +0530

    Committing paytm related files with pakacge example

--- LaviePolaniBag.java
+++
@@ -1,9 +0,0 @@
-class LaviePolaniBag{
-public static void main(String[] bag){
-System.out.println("Synthetic material grey colored hobo");
-System.out.println("35 centimeters height x 35 centimeters length x 13 centimeters width");
-System.out.println("Zip closure");
-System.out.println("1 compartment and 4 pockets");
-System.out.println("Trendy hobo bag with a smooth hand fee");
-}
-}
---
+++ paytm-system/src/com/xworkz/paytmapp/PaytmRunner.java
@@ -0,0 +1,7 @@
+package com.xworkz.paytmapp;
+
+public class PaytmRunner {
+    public static void main(String[] args) {
+        System.out.println("Paytm makes payment easier");
+    }
+}
